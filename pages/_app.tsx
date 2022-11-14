0846d34a7f739be269f20360a22741b3ed833a20
amtn55@mail.ru <amtn55@mail.ru>
2022-11-15 00:22:26 +0300

    .

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import type { AppProps } from "next/app";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+import Head from "next/head";
+import ThirdwebGuideFooter from "../components/GitHubLink";
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Polygon;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>Space Minting Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Space Minting Page"
+        />
+        <meta
+          name="keywords"
+          content="Space Minting Page"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <ThirdwebGuideFooter />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
